9b3aeb352b26e4281d50a217c23a6437d0fcf5e1
RoLYroLLs <github@rolyrolls.com>
2015-12-03 23:19:44 -0500

    created a new scaffolding for the ApplicationUser

---
+++ IdentityUserListSample/Models/UsersViewModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace IdentityUserListSample.Models {
+	public class UsersViewModel {
+		public string Id { get; set; }
+
+		[Display(Name = "UserName")]
+		public string UserName { get; set; }
+
+		[Display(Name = "Roles")]
+		public string Roles { get; set; }
+	}
+}
