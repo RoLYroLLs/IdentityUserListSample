1efc86e1a5d5df430390bf15d042536c92da9736
RoLYroLLs <github@rolyrolls.com>
2015-12-03 22:16:42 -0500

    initial project creation

---
+++ IdentityUserListSample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IdentityUserListSample.Startup))]
+namespace IdentityUserListSample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
